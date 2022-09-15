932f4ebb46d86f4a3478b11c4b6452e87c6a2092
Nikita Maslov <nikita.maslov@wirenboard.ru>
2022-09-15 16:33:07 +0300

    uid: fix python3 support + remove python2 build

--- wb_common/uid.py
+++ wb_common/uid.py
@@ -1,6 +1,5 @@
-from __future__ import print_function
-import re
 import os
+import re
 import subprocess
 
 # get_mac() has been moved to test_suite, because it uses wb-gen-serial,
@@ -8,55 +7,60 @@ import subprocess
 
 
 def get_cpuinfo_serial():
-    data = open('/proc/cpuinfo').read()
-    matches = re.findall('^Serial\s+: ([0-9a-f]+)$', data, re.M)
+    with open("/proc/cpuinfo", encoding="utf-8") as f:
+        data = f.read()
+    matches = re.findall("^Serial\s+: ([0-9a-f]+)$", data, re.M)
     if len(matches) > 0:
         return matches[0]
     return None
 
 
 def _devmem(address):
-    r = subprocess.check_output('busybox devmem 0x{:x} 32'.format(address), shell=True)
+    r = subprocess.check_output("busybox devmem 0x{:x} 32".format(address), shell=True)
     return int(r, 16)
 
 
 def get_wb7_cpu_serial():
-    r3 = _devmem(0x1c1b20c)
-    r2 = _devmem(0x1c1b208)
-    r1 = _devmem(0x1c1b204)
+    r3 = _devmem(0x1C1B20C)
+    r2 = _devmem(0x1C1B208)
+    r1 = _devmem(0x1C1B204)
 
-    return format(r3, '08x') + format(r2, '08x') + format((r1 >> 16) & 0xFFFF, '04x')
+    return format(r3, "08x") + format(r2, "08x") + format((r1 >> 16) & 0xFFFF, "04x")
 
 
 def get_mmc_serial():
-    mmc_prefix = '/sys/class/mmc_host/mmc0/'
+    mmc_prefix = "/sys/class/mmc_host/mmc0/"
     if os.path.exists(mmc_prefix):
         for entry in os.listdir(mmc_prefix):
-            if entry.startswith('mmc'):
-                serial_fname = mmc_prefix + entry + '/serial'
+            if entry.startswith("mmc"):
+                serial_fname = mmc_prefix + entry + "/serial"
                 if os.path.exists(serial_fname):
-                    serial = open(serial_fname).read().strip()
-                    if serial.startswith('0x'):
+                    with open(serial_fname, encoding="utf-8") as f:
+                        serial = f.read().strip()
+                    if serial.startswith("0x"):
                         serial = serial[2:]
                     return serial
     return None
 
-def get_eth_mac(num = 0):
+
+def get_eth_mac(num=0):
     netdev_path = os.path.realpath("/sys/class/net/eth" + str(num))
     of_node_path = os.path.realpath(netdev_path + "/../../of_node")
 
     mac_path = of_node_path + "/local-mac-address"
     if os.path.exists(mac_path):
-        mac = bytearray(open(mac_path).read(6))
+        with open(mac_path, "rb") as f:
+            mac = bytearray(f.read(6))
 
         # Check if default address was set by U-Boot (no EEPROM and unprogrammed OTP)
         if mac[:3] == bytearray([0x00, 0x04, 0x00]):
             return None
 
-        return ''.join(map(lambda b: format(b, "02x"), mac))
+        return "".join(map(lambda b: format(b, "02x"), mac))
 
     return None
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     print("/proc/cpuinfo serial: ", get_cpuinfo_serial())
-    #print "WB serial (eth mac): ", get_mac()
+    # print "WB serial (eth mac): ", get_mac()
