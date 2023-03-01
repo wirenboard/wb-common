863bfb09042fc5d6f0f4202f2b04efc17f23f3a1
Nikolay Korotkiy <sikmir@disroot.org>
2023-03-02 00:30:17 +0300

    Rename connect/disconnect to start/stop

--- wb_common/mqtt_client.py
+++ wb_common/mqtt_client.py
@@ -8,8 +8,9 @@ DEFAULT_BROKER_URL = "unix:///var/run/mosquitto/mosquitto.sock"
 
 
 class MQTTClient(paho_socket.Client):
-    def __init__(self, client_id_prefix: str, broker_url: str = DEFAULT_BROKER_URL):
+    def __init__(self, client_id_prefix: str, broker_url: str = DEFAULT_BROKER_URL, is_threaded: bool = True):
         self._broker_url = urlparse(broker_url)
+        self._is_threaded = is_threaded
         client_id = self.generate_client_id(client_id_prefix)
         transport = "websockets" if self._broker_url.scheme == "ws" else "tcp"
         super().__init__(client_id=client_id, transport=transport)
@@ -19,23 +20,25 @@ class MQTTClient(paho_socket.Client):
         random_suffix = "".join(random.sample(string.ascii_letters + string.digits, suffix_length))
         return "%s-%s" % (client_id_prefix, random_suffix)
 
-    def connect(self) -> None:
+    def start(self) -> None:
         scheme = self._broker_url.scheme
         if scheme == "unix":
             self.sock_connect(self._broker_url.path)
         elif scheme in ["mqtt-tcp", "tcp"]:
             if self._broker_url.username:
                 self.username_pw_set(self._broker_url.username, self._broker_url.password)
-            super().connect(self._broker_url.hostname, self._broker_url.port)
+            self.connect(self._broker_url.hostname, self._broker_url.port)
         elif scheme == "ws":
             if self._broker_url.path:
                 self.ws_set_options(self._broker_url.path)
-            super().connect(self._broker_url.hostname, self._broker_url.port)
+            self.connect(self._broker_url.hostname, self._broker_url.port)
         else:
             raise Exception("Unkown mqtt url scheme: " + scheme)
 
-        self.loop_start()
+        if self._is_threaded:
+            self.loop_start()
 
-    def disconnect(self) -> None:
-        self.loop_stop()
-        super().disconnect()
+    def stop(self) -> None:
+        if self._is_threaded:
+            self.loop_stop()
+        self.disconnect()
