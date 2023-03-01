fcb7f5117cf82865afce24a9030a4c715449bdbf
Nikolay Korotkiy <sikmir@disroot.org>
2023-03-02 01:40:05 +0400

    Add MQTT client wrapper (#8)

---
+++ wb_common/mqtt_client.py
@@ -0,0 +1,44 @@
+import random
+import string
+from urllib.parse import urlparse
+
+import paho_socket
+
+DEFAULT_BROKER_URL = "unix:///var/run/mosquitto/mosquitto.sock"
+
+
+class MQTTClient(paho_socket.Client):
+    def __init__(self, client_id_prefix: str, broker_url: str = DEFAULT_BROKER_URL, is_threaded: bool = True):
+        self._broker_url = urlparse(broker_url)
+        self._is_threaded = is_threaded
+        client_id = self.generate_client_id(client_id_prefix)
+        transport = "websockets" if self._broker_url.scheme == "ws" else "tcp"
+        super().__init__(client_id=client_id, transport=transport)
+
+    @staticmethod
+    def generate_client_id(client_id_prefix: str, suffix_length: int = 8) -> str:
+        random_suffix = "".join(random.sample(string.ascii_letters + string.digits, suffix_length))
+        return "%s-%s" % (client_id_prefix, random_suffix)
+
+    def start(self) -> None:
+        scheme = self._broker_url.scheme
+        if scheme == "unix":
+            self.sock_connect(self._broker_url.path)
+        elif scheme in ["mqtt-tcp", "tcp"]:
+            if self._broker_url.username:
+                self.username_pw_set(self._broker_url.username, self._broker_url.password)
+            self.connect(self._broker_url.hostname, self._broker_url.port)
+        elif scheme == "ws":
+            if self._broker_url.path:
+                self.ws_set_options(self._broker_url.path)
+            self.connect(self._broker_url.hostname, self._broker_url.port)
+        else:
+            raise Exception("Unkown mqtt url scheme: " + scheme)
+
+        if self._is_threaded:
+            self.loop_start()
+
+    def stop(self) -> None:
+        if self._is_threaded:
+            self.loop_stop()
+        self.disconnect()
